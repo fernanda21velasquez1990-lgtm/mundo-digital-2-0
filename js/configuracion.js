c2eb2e8fa63987b1c2ba47c27d42d1c5b72b7d80
will21still1991-hub <will21still1991@gmail.com>
2026-08-09 15:49:57 -0400

    Restaura portal Mi cuenta estable

--- js/configuracion.js
+++ js/configuracion.js
@@ -6,7 +6,7 @@
  * y la clave API que aparecerá en CONFIGURACION > API_KEY_SOCIOS.
  */
 window.MUNDO_DIGITAL_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzlbNSOhEnIMk2kC9kg4yw1NdEtU9He_X0sAzb4-w7EWMVAF4EAU9_8327Zs45czbCM/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwf3HYsuDdN-FUwHBUugX_75GbT5Ds9_dridE81HHHpw0k-7tvG6X9g89S8Uw4A6Fg7/exec",
   API_KEY_SOCIOS: "c6beff9d014d4e9d840f4d8f86a39680d18092ef5e1c4891abda044336002241",
   DIAS_ALERTA_VENCIMIENTO: 3
 };
