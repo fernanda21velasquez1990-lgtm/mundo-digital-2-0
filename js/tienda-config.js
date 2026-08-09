510707a2d54f36bb3357e469df6e2c76b9ad73f1
will21still1991-hub <will21still1991@gmail.com>
2026-08-09 17:05:15 -0400

    Restaura web completa estable

--- js/tienda-config.js
+++ js/tienda-config.js
@@ -5,5 +5,5 @@
  * Este archivo NO contiene la clave privada del panel administrativo.
  */
 window.MD20_TIENDA_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzlbNSOhEnIMk2kC9kg4yw1NdEtU9He_X0sAzb4-w7EWMVAF4EAU9_8327Zs45czbCM/exec"
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwf3HYsuDdN-FUwHBUugX_75GbT5Ds9_dridE81HHHpw0k-7tvG6X9g89S8Uw4A6Fg7/exec"
 };
